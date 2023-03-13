29059aec0ddb2ba3b2f6e1f67f607ae3c24cadce
Thash9523b <thais_duarte03@outlook.com>
2023-03-12 21:08:20 -0300

    att4

--- aula 4/script.js
+++ aula 4/script.js
@@ -1,7 +1,6 @@
 const posts = [
     {
         title:"5SOS5",
-        // image: "https://i.pinimg.com/564x/32/71/a1/3271a110ce74a010b019dfa3bb3f7f5b.jpg",
         content:"The new album 5SOS5 is yours now <3 Click here to listen!"
     },
     {
@@ -25,11 +24,6 @@ for (var i=0;i<posts.length;i++){
     const titleText = document.createTextNode(postData.title);
     title.appendChild(titleText)
     article.appendChild(title);
-
-    // const image = document.createElement('img');
-    // image.classList.add('articleImg');
-    // image.src = postData.image;
-    // image.appendChild(image);
 
     const content = document.createElement('p');
     const contentText = document.createTextNode(postData.content);
